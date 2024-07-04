97c54dd7f161fb1d443419b876837bf0b35fe043
Moises Lopes Ferreira <moises@peakonedev.com>
2024-07-04 09:41:40 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+      <iframe src=" https://mostra.46graus.com">
+          {"Your browser doesn't support iframes  https://mostra.46graus.com"}
+      </iframe>
+  )
+}
